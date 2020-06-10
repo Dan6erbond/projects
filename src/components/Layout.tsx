a18cd04e6b5bd8667f061ca245e4a34c3f78f433
Dan6erbond <moravrav@gmail.com>
2020-06-10 07:03:28 +0200

    Fix formatting

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,18 +3,16 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 
 interface LayoutProps {
-    children?: React.ReactNode;
+  children?: React.ReactNode;
 }
 
 export default function Layout(props: LayoutProps) {
-    return (
-        <React.Fragment>
-            <Header/>
-            <main style={{minHeight: '100vh'}}>
-                {props.children}
-            </main>
-            <br/>
-            <Footer/>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <Header />
+      <main style={{ minHeight: "100vh" }}>{props.children}</main>
+      <br />
+      <Footer />
+    </React.Fragment>
+  );
 }
